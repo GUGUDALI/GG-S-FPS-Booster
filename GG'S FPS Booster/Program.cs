c5c515f8314d7fd2b5f4650dbbafdccce3c29cf9
Selimcan "GUGUDALI" YILDIZ <55906355+GUGUDALI@users.noreply.github.com>
2025-01-09 17:17:44 +0300

    Updated to v1.1

--- GG'S FPS Booster/Program.cs
+++ GG'S FPS Booster/Program.cs
@@ -1,170 +1,311 @@
-﻿using System.Diagnostics;
+﻿// Version: 1.1
+// Description: GG'S FPS Booster is a program that allows you to boost your CS2 FPS by changing the priority of the game process and setting the processor affinity.
+// Developer: GUGUDALI
+// GitHub: https://github.com/GUGUDALI/GG-S-FPS-Booster
+// Last update: 08/01/2025
+using System.Diagnostics;
 using System.Runtime.InteropServices;
 
 namespace GG_S_FPS_Booster
 {
     class ProcessList
     {
-        public int processID { get; set; }
-        public ProcessPriorityClass processPriorityClass { get; set; }
-        public bool priorityBoostEnabled { get; set; }
-        public nint processorAffinity { get; set; }
+        public int ProcessID { get; set; }
+        public ProcessPriorityClass ProcessPriorityClass { get; set; }
+        public bool PriorityBoostEnabled { get; set; }
+        public nint ProcessorAffinity { get; set; }
     }
-    internal class Program
+
+    class Program
     {
-        private static int idleCore;
-        private static int highCore;
-        private static Process[] processes;
-        private static int[] cores = new int[Environment.ProcessorCount];
-        private static Process fpsProcess;
-        private static Process proc;
-		private static ProcessList[] processList;
-        private static bool isCSFinded = false;
+        static int idleCore;
+        static int highCore;
+        static Process[]? processes;
+        static readonly int[] cores = new int[Environment.ProcessorCount];
+        static Process? csProcess;
+        static Process? process;
+        static ProcessList[]? processList;
+        static bool iscsFinded = false;
+        static readonly string[] exList = {"cs2", "csrss", "Idle", "Registry", "SecurityHealthService", "services", "SgrmBroker", "smss", "System", "wininit"};
+        static readonly string title = "GG'S FPS Booster v1.1 by GUGUDALI";
+        static readonly bool isWindows = RuntimeInformation.IsOSPlatform(OSPlatform.Windows);
 
         static void Main(string[] args)
         {
-			Console.Title = "GG'S FPS Booster v1.0 by GUGUDALI";
-            for (int i = 0; i < cores.Length; i++)
+            try
             {
-                if (i == 0)
+                InitializeApp();
+                while (true)
                 {
-                    cores[i] = 1;
-                }
-                else
-                {
-                    cores[i] = cores[i - 1] + cores[i - 1];
+                    WaitForCS();
+                    Thread.Sleep(500);
+                    BoostCS();
+                    Thread.Sleep(500);
+                    WaitCSClose();
+                    Thread.Sleep(500);
+                    RestoreSettings();
+                    Thread.Sleep(3000);
                 }
             }
-
-            for (int i = 0; i < cores.Length; i++)
+            catch (Exception ex)
             {
-                if (i == 0 || i == 1 || i == (cores.Length / 2) || i == cores.Length / 2 + 1)
-                {
-                    
-                }
-                else
-                {
-                    idleCore += cores[i];
-                }
+                Console.WriteLine(ex.Message);
             }
+        }
 
-            for (int i = 0; i < cores.Length; i++)
+        // Restore the processor affinity and priority of the processes
+        static void RestoreSettings()
+        {
+            try
             {
-                highCore += cores[i];
-            }
-
-            while(true){
-                try
+                if (processList != null)
                 {
-                    Console.Clear();
-                    Console.BackgroundColor = ConsoleColor.Black;
-                    Console.ForegroundColor = ConsoleColor.White;
-                    Console.WriteLine("GG'S FPS Booster v1.0 by GUGUDALI\n\n");
-
-                    Console.WriteLine("Waiting for CS2 to start...");
-                    while (!isCSFinded){
-                        processes = Process.GetProcesses();
-                        for (int i = 0; i < processes.Length; i++)
+                    WriteConsole("Restoring Settings...", ConsoleColor.Yellow);
+                    processes = Process.GetProcesses();
+                    foreach (var process in processes)
+                    {
+                        try
                         {
-                            try
-                            {
-                                proc = processes[i];
-                                if (proc.ProcessName == "cs2"){
-                                    fpsProcess = proc;
-                                    isCSFinded = true;
-                                    Console.Clear();
-                                    Console.WriteLine("GG'S FPS Booster v1.0 by GUGUDALI\n\n");
-                                    Console.WriteLine("CS2 found. ID : " + fpsProcess.Id);
-                                }
-                            }catch{}
+                            foreach (var exProcess in processList){
+                                try
+                                {
+                                    if (exProcess.ProcessID==process.Id){
+                                        process.PriorityClass = exProcess.ProcessPriorityClass;
+                                        process.PriorityBoostEnabled = exProcess.PriorityBoostEnabled;
+                                        if (!exList.Contains(process.ProcessName))
+                                        {
+                                            process.ProcessorAffinity = exProcess.ProcessorAffinity;
+                                        }
+                                    }
+                                }catch{}
+                            }
                         }
-                        Thread.Sleep(2000);
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine(ex.Message);
+                        }
                     }
+                    csProcess = null;
+                    processList = null;
+                    iscsFinded = false;
+                    WriteConsole("Settings restored.", ConsoleColor.Yellow);
+                    Thread.Sleep(3000);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
 
-                    Console.Clear();
-                    Console.WriteLine("GG'S FPS Booster v1.0 by GUGUDALI\n\n");
-                    Console.WriteLine("Boosting CS2's FPS...");
+        // Wait for the CS2 process to close
+        static void WaitCSClose()
+        {
+            if (csProcess != null)
+            {
+                csProcess.WaitForExit();
+                WriteConsole("CS2 closed.\n\nResetting Settings...", ConsoleColor.White);
+            }
+            else iscsFinded = false;
+        }
+
+        // Boost the CS2 process
+        static void BoostCS()
+        {
+            try
+            {
+                if (csProcess != null)
+                {
+                    WriteConsole("Boosting CS2's FPS...", ConsoleColor.Green);
 
                     processes = Process.GetProcesses();
                     processList = new ProcessList[processes.Length];
-                    
-                    for (int i = 0; i < processes.Length; i++)
-                    {
-                        try
-                        {
-                            proc = processes[i];
-                                try
-                                {
-                                    if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows) || RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
-                                    {
-                                        processList[i] = new ProcessList{
-                                            processID = proc.Id,
-                                            processPriorityClass = proc.PriorityClass,
-                                            priorityBoostEnabled = proc.PriorityBoostEnabled,
-                                            processorAffinity = proc.ProcessorAffinity
-                                        };
-                                    }else{
-                                        processList[i] = new ProcessList{
-                                            processID = proc.Id,
-                                            processPriorityClass = proc.PriorityClass,
-                                            priorityBoostEnabled = proc.PriorityBoostEnabled
-                                        };
-                                    }
-                                }catch{}
-
-                                proc.PriorityClass = ProcessPriorityClass.Idle;
-                                proc.PriorityBoostEnabled = false;
-                                if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows) || RuntimeInformation.IsOSPlatform(OSPlatform.Linux) && proc.ProcessName != "svchost" && proc.ProcessName != "System" && proc.ProcessName != "Idle" && proc.ProcessName != "csrss" && proc.ProcessName != "smss" && proc.ProcessName != "Registry" && proc.ProcessName != "wininit" && proc.ProcessName != "winlogon" && proc.ProcessName != "lsass" && proc.ProcessName != "lsm" && proc.ProcessName != "cs2")
-                                {
-                                    proc.ProcessorAffinity = (IntPtr)idleCore;
-                                }
-                        }catch{}
-                    }
-
-                    fpsProcess.PriorityClass = ProcessPriorityClass.RealTime;
-                    fpsProcess.PriorityBoostEnabled = true;
-                    if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows) || RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
-                    {
-                        fpsProcess.ProcessorAffinity = (IntPtr)highCore;
-                    }
-
-                    Console.Clear();
-                    Console.ForegroundColor = ConsoleColor.Green;
-                    Console.WriteLine("GG'S FPS Booster v1.0 by GUGUDALI\n\n");
-                    Console.WriteLine("CS2's FPS boosted.\n\n");
-                    Console.WriteLine("Waiting for CS2 to close...");
-                    fpsProcess.WaitForExit();
-
-                    Console.Clear();
-                    Console.WriteLine("GG'S FPS Booster v1.0 by GUGUDALI\n\n");
-                    Console.WriteLine("CS2 closed. Restoring settings...");
 
                     for (int i = 0; i < processes.Length; i++)
                     {
                         try
                         {
-                            proc = processes[i];
-                                for (int j = 0; j < processList.Length; j++)
+                            process = processes[i];
+                            try
+                            {
+                                processList[i] = new ProcessList
                                 {
-                                    try
-                                    {
-                                        if (proc.Id == processList[j].processID)
-                                        {
-                                            proc.PriorityClass = processList[j].processPriorityClass;
-                                            proc.PriorityBoostEnabled = processList[j].priorityBoostEnabled;
-                                            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows) || RuntimeInformation.IsOSPlatform(OSPlatform.Linux) && proc.ProcessName != "svchost" && proc.ProcessName != "System" && proc.ProcessName != "Idle" && proc.ProcessName != "csrss" && proc.ProcessName != "smss" && proc.ProcessName != "Registry" && proc.ProcessName != "wininit" && proc.ProcessName != "winlogon" && proc.ProcessName != "lsass" && proc.ProcessName != "lsm" && proc.ProcessName != "cs2")
-                                            {
-                                                proc.ProcessorAffinity = processList[j].processorAffinity;
-                                            }
-                                        }
-                                    }catch{}
+                                    ProcessID = process.Id,
+                                    ProcessPriorityClass = process.PriorityClass,
+                                    PriorityBoostEnabled = process.PriorityBoostEnabled,
+                                    ProcessorAffinity = process.ProcessorAffinity
+                                };
+                            }
+                            catch (Exception ex)
+                            {
+                                Console.WriteLine(ex.Message);
+                            }
+                            Console.WriteLine($"{processList[i].ProcessID.ToString()} {processList[i].ProcessPriorityClass.ToString()} {processList[i].PriorityBoostEnabled.ToString()} {processList[i].ProcessorAffinity.ToString()}");
+                            try
+                            {
+                                process.PriorityClass = ProcessPriorityClass.Idle;
+                                process.PriorityBoostEnabled = false;
+                                if (process.ProcessName != "csrss" && process.ProcessName != "Idle" && process.ProcessName != "Registry" && process.ProcessName != "SecurityHealthService" && process.ProcessName != "services" && process.ProcessName != "SgrmBroker" && process.ProcessName != "smss" && process.ProcessName != "System" && process.ProcessName != "wininit" && process.ProcessName != "cs2")
+                                {
+                                    process.ProcessorAffinity = (IntPtr)idleCore;
                                 }
-                        }catch{}
-                    }
+                            }
+                            catch (Exception ex)
+                            {
+                                Console.WriteLine(ex.Message);
+                            }
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine(ex.Message);
+                        }
+                    };
+                    Console.ReadLine();
+                    csProcess.PriorityClass = ProcessPriorityClass.RealTime;
+                    csProcess.PriorityBoostEnabled = true;
+                    csProcess.ProcessorAffinity = (IntPtr)highCore;
+
+                    WriteConsole("CS2's FPS boosted.\n\nWaiting for CS2 to close...", ConsoleColor.Green);
+                }
+                else
+                {
+                    iscsFinded = false;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        // Wait for the CS2 process
+        static void WaitForCS()
+        {
+            try
+            {
+                while (!iscsFinded)
+                {
+                    WriteConsole("Waiting for CS2...", ConsoleColor.Cyan);
+
+                    processes = Process.GetProcesses();
+                    csProcess = processes.FirstOrDefault(p => p.ProcessName == "cs2");
+                    iscsFinded = csProcess != null;
+
                     Thread.Sleep(3000);
-                    isCSFinded = false;
-                    processList = null;
-                }catch{}
+                }
+                if (csProcess != null)
+                {
+                    WriteConsole($"CS2 found! PID: {csProcess.Id}", ConsoleColor.Cyan);
+                }
+                Thread.Sleep(3000);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        // Initialize the application
+        static void InitializeApp()
+        {
+            try
+            {
+                AppDomain.CurrentDomain.ProcessExit += OnClosing;
+                CheckWindows();
+                Console.Title = title;
+                for (int i = 0; i < cores.Length; i++)
+                {
+                    if (i == 0)
+                    {
+                        cores[i] = 1;
+                    }
+                    else
+                    {
+                        cores[i] = cores[i - 1] + cores[i - 1];
+                    }
+                }
+                if (cores.Length < 5)
+                {
+                    for (int i = 0; i < cores.Length; i++)
+                    {
+                        if (i == 0 || i == (cores.Length / 2))
+                        {
+
+                        }
+                        else
+                        {
+                            idleCore += cores[i];
+                        }
+                    }
+                }
+                else
+                {
+                    for (int i = 0; i < cores.Length; i++)
+                    {
+                        if (i == 0 || i == 1 || i == (cores.Length / 2) || i == cores.Length / 2 + 1)
+                        {
+
+                        }
+                        else
+                        {
+                            idleCore += cores[i];
+                        }
+                    }
+                }
+                for (int i = 0; i < cores.Length; i++)
+                {
+                    highCore += cores[i];
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        // Reset the processor affinity and priority of the processes on closing
+        static void OnClosing(object? sender, EventArgs e)
+        {
+            try
+            {
+                RestoreSettings();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        // Check if the program is running on Windows
+        static void CheckWindows()
+        {
+            try
+            {
+                if (!isWindows)
+                {
+                    WriteConsole("This program is only available for Windows.\nPress any key to exit...", ConsoleColor.Red);
+                    Console.ReadKey();
+                    Environment.Exit(0);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        // Write the text to the console
+        static void WriteConsole(string text, ConsoleColor color)
+        {
+            try
+            {
+                Console.Clear();
+                Console.ForegroundColor = color;
+                Console.WriteLine($"{title}\n\n{text}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
             }
         }
     }
